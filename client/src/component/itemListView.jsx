6652ce1bbe024bd874ef0e3d9e10e22cac74dd73
Angie Tang <atang167@gmail.com>
2017-09-26 15:38:58 -0700

    Add fridge, items list, and item view.

---
+++ client/src/component/itemListView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import itemListEntry from './itemListEntry'
+
+const itemList = (props) => {
+  return (
+    <div>
+    {props.map(item => (
+      <ItemListEntry name={item.name} quantity={item.quantity} type={item.type}/>
+    ))}
+    </div> 
+  )
+}
+
+export default itemList; 
